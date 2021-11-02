9a4e24649e8c536f5105ebfd188052f4ee215782
root <root@peter.localdomain>
2021-11-02 19:36:31 +0900

    feat: searchview(검색) 구현

--- posts/models.py
+++ posts/models.py
@@ -4,11 +4,18 @@ from core.models import TimeStamp
 from users.models import User
 
 
+class Category(TimeStamp):
+    name = models.CharField(max_length=20, null=False)
+
+    class Meta:
+        db_table = 'categories'
+
 class Post(TimeStamp):
     title      = models.CharField(max_length=100, null=False)
     content    = models.TextField(null=False)
     author     = models.ForeignKey(User, on_delete=models.CASCADE)
     view_count = models.PositiveIntegerField(default=0, verbose_name='게시글 조회수')
+    category   = models.ForeignKey('Category', on_delete=models.CASCADE)
     
     class Meta:
         db_table = 'posts'
--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,8 @@ from posts.views import (
     PostDetailView, 
     PostManageView,
     PostListView,
-    PostView
+    PostView,
+    SearchView
 )
 
 urlpatterns = [
@@ -11,4 +12,5 @@ urlpatterns = [
     path('/main', PostListView.as_view()),
     path('/newpost', PostView.as_view()),
     path('/post/manage/<int:post_id>', PostManageView.as_view()),
+    path('/post/search', SearchView.as_view()),
 ]
